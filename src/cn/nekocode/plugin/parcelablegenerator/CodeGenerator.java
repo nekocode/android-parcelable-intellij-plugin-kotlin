d57e9fe3515aaa536b7e268523c26694badb24c5
nekocode <syfyw@qq.com>
2016-02-02 10:42:48 +0800

    updated readme and license

--- src/cn/nekocode/plugin/parcelablegenerator/CodeGenerator.java
+++ src/cn/nekocode/plugin/parcelablegenerator/CodeGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Michał Charmas (http://blog.charmas.pl)
+ * Copyright (C) 2016 Nekocode (https://github.com/nekocode)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
